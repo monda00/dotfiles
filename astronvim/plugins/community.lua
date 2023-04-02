b90af23f30724ca604e187df092e446d6ac216fc
monda00 <monda0524@gmail.com>
2023-04-02 14:37:46 +0900

    add astronvim plugin, community and ts

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -1,9 +1,49 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox", enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
---
+++ astronvim/plugins/typescript.lua
@@ -0,0 +1,146 @@
+local utils = require "astrocommunity.utils"
+local events = require "neo-tree.events"
+local function on_file_remove(args)
+  local ts_clients = vim.lsp.get_active_clients { name = "tsserver" }
+  for _, ts_client in ipairs(ts_clients) do
+    ts_client.request("workspace/executeCommand", {
+      command = "_typescript.applyRenameFile",
+      arguments = {
+        {
+          sourceUri = vim.uri_from_fname(args.source),
+          targetUri = vim.uri_from_fname(args.destination),
+        },
+      },
+    })
+  end
+end
+
+return {
+  { import = "astrocommunity.pack.json" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the required file types to opts.ensure_installed.
+      utils.list_insert_unique(opts.ensure_installed, { "javascript", "typescript", "tsx" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add tsserver to opts.ensure_installed using vim.list_extend.
+      utils.list_insert_unique(opts.ensure_installed, "tsserver")
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using vim.list_extend.
+      utils.list_insert_unique(opts.ensure_installed, "prettierd")
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, "js")
+    end,
+  },
+  {
+    "vuki656/package-info.nvim",
+    requires = "MunifTanjim/nui.nvim",
+    config = true,
+    event = "BufRead package.json",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    ft = { "ts", "js", "tsx", "jsx" },
+    enabled = true,
+    dependencies = {
+      {
+        "mxsdev/nvim-dap-vscode-js",
+        opts = { debugger_cmd = { "js-debug-adapter" }, adapters = { "pwa-node" } },
+      },
+      { "theHamsta/nvim-dap-virtual-text", config = true },
+      { "rcarriga/nvim-dap-ui", config = true },
+    },
+    config = function()
+      local dap = require "dap"
+
+      local attach_node = {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach",
+        processId = function(...) return require("dap.utils").pick_process(...) end,
+        cwd = "${workspaceFolder}",
+      }
+
+      dap.configurations.javascript = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        attach_node,
+      }
+      dap.configurations.typescript = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          runtimeExecutable = "ts-node",
+          sourceMaps = true,
+          protocol = "inspector",
+          console = "integratedTerminal",
+          resolveSourceMapLocations = {
+            "${workspaceFolder}/dist/**/*.js",
+            "${workspaceFolder}/**",
+            "!**/node_modules/**",
+          },
+        },
+        attach_node,
+      }
+    end,
+  },
+  {
+    "jose-elias-alvarez/typescript.nvim",
+    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "tsserver") end,
+    ft = {
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "javascriptreact",
+    },
+    opts = function() return { server = require("astronvim.utils.lsp").config "tsserver" } end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = events.FILE_MOVED,
+          handler = on_file_remove,
+        },
+        {
+          event = events.FILE_RENAMED,
+          handler = on_file_remove,
+        },
+      },
+    },
+  },
+}
