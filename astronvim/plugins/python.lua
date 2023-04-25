c2d9eeca5bb7dfde46c75b8e822b2c5f2e48b0dd
monda00 <monda0524@gmail.com>
2023-04-25 21:06:09 +0900

    update alias and python config

---
+++ astronvim/plugins/python.lua
@@ -0,0 +1,48 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the "python" and "toml" language to opts.ensure_installed.
+      utils.list_insert_unique(opts.ensure_installed, { "python", "toml" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add pyright lsp to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, { "pyright", "ruff_lsp" })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using vim.list_extend.
+      utils.list_insert_unique(opts.ensure_installed, { "isort", "black" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, "python")
+    end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    opts = {},
+    keys = { { "<leader>lv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  },
+}
